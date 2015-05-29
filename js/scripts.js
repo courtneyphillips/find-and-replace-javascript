9bfe3467d944fe26bc53ff9d464f3e7069cdcee9
Courtney <courtney.mae.phillips@gmail.com>
2015-05-29 10:34:27 -0700

    Add jQuery and basic CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 var findAndReplace = function(phrase, word, replacement) {
   return phrase.replace(new RegExp(word, "gi"),replacement);
 }
+
+$(document).ready(function(){
+
+  $("#findreplace").submit(function(event) {
+
+    var phrase = $("input#phrase").val();
+    var word = $("input#word").val();
+    var replacement = $("input#replacement").val();
+    var output = findAndReplace(phrase, word, replacement);
+
+    $(".output").text(output);
+    $("#result").slideToggle("slow");
+
+    event.preventDefault();
+
+  });
+
+});
